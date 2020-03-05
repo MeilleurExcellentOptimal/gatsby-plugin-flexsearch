d4dba9a648d095302b5c88313a2e59bb1c784af0
MeilleurExcellentOptimal <61599859+MeilleurExcellentOptimal@users.noreply.github.com>
2020-03-06 07:04:33 +0800

    Update gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,95 +1,95 @@
-const _ = require('lodash')
-const fs = require('fs')
+const _ = require("lodash");
+const fs = require("fs");
 // set flexsearch object as a global variable to make it available to language files
-global.FlexSearch = require('flexsearch')
+global.FlexSearch = require("flexsearch");
 
 exports.onPostBootstrap = function(_ref, options) {
-  const { getNodes } = _ref
+  const { getNodes } = _ref;
 
-  const { type } = options
+  const { type } = options;
 
-  const _options$langua = options.languages
-  const languages = _options$langua === undefined ? ['en'] : _options$langua
+  const _options$langua = options.languages;
+  const languages = _options$langua === undefined ? ["en"] : _options$langua;
 
-  const _options$fields = options.fields
-  const fields = _options$fields === undefined ? [] : _options$fields
+  const _options$fields = options.fields;
+  const fields = _options$fields === undefined ? [] : _options$fields;
 
-  const store = []
-  const indexStore = []
-  const fullIndex = {}
+  const store = [];
+  const indexStore = [];
+  const fullIndex = {};
 
   languages.forEach(lng => {
     // collect fields to store
     const fieldsToStore = fields
       .filter(field => (field.store ? field.resolver : null))
-      .map(field => ({ name: field.name, resolver: field.resolver }))
-    const nid = []
+      .map(field => ({ name: field.name, resolver: field.resolver }));
+    const nid = [];
 
     // add each field to index
     fields.forEach(index_ => {
-      const index = {}
-      index.name = index_.name
+      const index = {};
+      index.name = index_.name;
 
       if (index_.indexed) {
-        const attrs = index_.attributes
-        index.attrs = attrs
+        const attrs = index_.attributes;
+        index.attrs = attrs;
 
         // load language files if needed by stemmer or filter
         if (
-          index_.attrs.stemmer !== undefined ||
-          index_.attrs.filter !== undefined
+          index.attrs.stemmer !== undefined ||
+          index.attrs.filter !== undefined
         ) {
           try {
-            if (lng === 'en') {
-              require('./lang/en')
-            } else if (lng === 'de') {
-              require('./lang/en')
+            if (lng === "en") {
+              require("./lang/en");
+            } else if (lng === "de") {
+              require("./lang/de");
             } else {
               console.error(
-                'Language not supported by pre-defined stemmer or filter'
-              )
+                "Language not supported by pre-defined stemmer or filter"
+              );
             }
           } catch (e) {
-            console.error(e)
+            console.error(e);
           }
         }
 
-        index.values = new FlexSearch(attrs)
+        index.values = new FlexSearch(attrs);
       }
 
       getNodes()
         .filter(node => {
           if (node.internal.type === type) {
-            return node
+            return node;
           }
         })
         .forEach((n, i) => {
-          const id = i
+          const id = i;
           if (index_.indexed) {
-            const content = _.get(n, index_.resolver)
-            index.values.add(id, content)
+            const content = _.get(n, index_.resolver);
+            index.values.add(id, content);
           }
-          const nodeContent = {}
+          const nodeContent = {};
           fieldsToStore.forEach(field => {
-            nodeContent[field.name] = _.get(n, field.resolver)
-          })
+            nodeContent[field.name] = _.get(n, field.resolver);
+          });
           if (!nid.includes(id)) {
-            store.push({ id, node: nodeContent })
-            nid.push(id)
+            store.push({ id, node: nodeContent });
+            nid.push(id);
           }
-        })
+        });
 
       if (index_.indexed) {
-        index.values = index.values.export()
-        indexStore.push(index)
+        index.values = index.values.export();
+        indexStore.push(index);
       }
-    })
+    });
 
     fullIndex[lng] = {
       index: indexStore,
-      store,
-    }
-  })
+      store
+    };
+  });
 
-  fs.writeFileSync('public/flexsearch_index.json', JSON.stringify(fullIndex))
-}
+  fs.writeFileSync("public/flexsearch_index.json", JSON.stringify(fullIndex));
+};
